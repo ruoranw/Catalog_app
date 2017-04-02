5df866feab5c87768c8a1743a120329a3de87357
WangRuoran <wangruoran@WangRuorans-MacBook-Pro.local>
2017-04-01 17:40:18 -0700

    table, view, trigger, uncomplete,passes 6 tests.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,26 +13,46 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT count(*) FROM players;")
+    result = c.fetchone()[0]
+    conn.commit()
+    conn.close()
+    return result
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s)",(name,))
+    conn.commit()
+    conn.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,6 +67,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM complete;")
+    results = c.fetchall()
+    conn.commit()
+    conn.close()
+    return results
 
 
 def reportMatch(winner, loser):
@@ -56,16 +83,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches (winner,loser) VALUES (%s,%s)",(winner,loser))
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,56 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+DROP DATABASE IF EXISTS tournament；
+
+
+CREATE TABLE players(id serial PRIMARY KEY,
+                     name text);
+
+
+CREATE TABLE matches(match_id serial PRIMARY KEY,
+                     winner integer REFERENCES players(id),
+                     loser integer REFERENCES players(id));
+
+
+CREATE VIEW complete AS SELECT players.id, players.name,
+                     COUNT (matches.winner) as wins,
+                     COUNT (matches.loser) + COUNT(matches.loser) as total_m
+                     FROM players LEFT JOIN matches ON id = winner AND id = loser
+                     GROUP BY players.id
+                     ORDER BY wins DESC;
+
+
+CREATE OR REPLACE FUNCTION updated_complete()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $BODY$
+  BEGIN
+      IF TG_OP = 'UPDATE' THEN
+          UPDATE complete SET wins = wins+1, total_m = total_m+1 WHERE id = NEW.id;
+          UPDATE complete SET loses = loses+1, total_m = total_m+1 WHERE id = NEW.id;
+          RETURN NEW;
+      END IF;
+      RETURN NEW;
+  END;
+  $BODY$;
+
+CREATE TRIGGER updated_complete_trig
+    INSTEAD OF UPDATE
+    ON complete FOR EACH ROW EXECUTE PROCEDURE updated_complete();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
