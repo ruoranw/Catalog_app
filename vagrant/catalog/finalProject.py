f7d52c3666667103402e3343a3965ee4168ec428
WangRuoran <wangruoran@WangRuorans-MacBook-Pro.local>
2017-05-05 21:56:15 -0700

    Add login

--- vagrant/catalog/finalProject.py
+++ vagrant/catalog/finalProject.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect, url_for,flash , jsonify
+from flask import Flask,render_template, request, redirect, url_for,flash, jsonify
 app = Flask(__name__)
 
 # Import CRUD Operations from Lesson 1
@@ -6,12 +6,135 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+# New import for creating anti-forgery state token
+
+from flask import session as login_session
+import random, string
+
+# New import for Gconnect
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID =json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
 # Create an instance of this class with the name of running application as the argument
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind=engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
+
+# Create a state token to prevent request forgery.
+# Store it in the session for later validation.
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    # Render the login template
+    return render_template('login.html', STATE=state)
+    # return "The current session state is %s" %login_session['state']
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+  # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('http://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+  # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+ # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+   # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+  # Check to see if the user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                   200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+  # Store the access token in the session for later use.
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+  # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+# Create an API Endpoint (Get Request)
+@app.route('/restaurants/JSON')
+def restaurantJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(Restaurant=[r.serialize for r in restaurants])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItem=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItem(restaurant_id, menu_id):
+    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=menuItem.serialize)
+
 
 # Add Routes
 
@@ -31,7 +154,8 @@ def newRestaurant():
         newRestaurant = Restaurant(name = request.form['name'])
         session.add(newRestaurant)
         session.commit()
-        return redirect(url_for('showRestaurants', restaurants = restaurants))
+        flash("New restaurant is created!")
+        return redirect(url_for('showRestaurants'))
     else:
         return render_template('newRestaurant.html')
 
@@ -44,67 +168,102 @@ def editRestaurant(restaurant_id):
     editedRestaurant = session.query(Restaurant).filter_by(id =restaurant_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            editRestaurant.name = request.form['name']
-            session.add(editRestaurant)
-            session.commit()
-            return redirect(url_for('showRestaurants'))
+            editedRestaurant.name = request.form['name']
+        session.add(editedRestaurant)
+        session.commit()
+        flash("The restaurant is edited successfully!")
+        return redirect(url_for('showRestaurants'))
     else:
-        return render_template('editRestaurant.html', restaurant=editedRestaurant,restaurant_id=restaurant_id)
+        return render_template('editRestaurant.html', restaurant = editedRestaurant)
 
     # return "This page will be for editing the %s restaurant." %restaurant_id
 
 
-@app.route('/restaurants/3/delete/')
+@app.route('/restaurants/<int:restaurant_id>/delete/', methods=['GET','POST'])
 # Delete a restaurants
-def deleteRestaurant():
-
-    return render_template('deleteRestaurant.html')
+def deleteRestaurant(restaurant_id):
+    restaurantToDelete= session.query(Restaurant).filter_by(id =restaurant_id).one()
+    if request.method == 'POST':
+        session.delete(restaurantToDelete)
+        session.commit()
+        flash("The restaurant is deleted successfully!")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('deleteRestaurant.html', restaurant = restaurantToDelete)
     # return "This page will be for deleting the %s restaurant." %restaurant_id
 
 
-@app.route('/restaurants/restaurant_id/')
-@app.route('/restaurants/3/menu/')
+@app.route('/restaurants/<int:restaurant_id>/', methods=['GET','POST'])
+@app.route('/restaurants/<int:restaurant_id>/menu/', methods=['GET','POST'])
 # Show all menu items of a restaurant
-def showMenu():
-
-    return render_template('menu.html')
+def showMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    menus = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return render_template('menu.html', restaurant = restaurant, menus = menus, restaurant_id = restaurant_id)
     # return "This page is for the menu of restaurant %s." %restaurant_id
 
 
-@app.route('/restaurants/3/menu/new')
+@app.route('/restaurants/<int:restaurant_id>/menu/new/', methods=['GET','POST'])
 # Make a new menu
-def newMenuItem():
-    return render_template('newMenuItem.html')
+def newMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'],description = request.form['description'], price = request.form['price'], course = request.form['course'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        flash("New menu item is created!")
+        return redirect(url_for('showMenu',restaurant_id=restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id = restaurant_id)
     # return "This page is for making a new menu item for restaurant %s." %restaurant_id
 
 
-@app.route('/restaurants/restaurant_id/menu/menu_id/edit')
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET','POST'])
 # Edit the menu
-def editMenuItem():
+def editMenuItem(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    item = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            item.name = request.form['name']
+        if request.form['description']:
+            item.description = request.form['description']
+        if request.form['price']:
+            item.price = request.form['price']
+        if request.form['course']:
+            item.course = request.form['course']
 
-    return render_template('editMenuItem.html')
+        session.add(item)
+        session.commit()
+        flash("This menu item is successfully edited!")
+
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+
+    else:
+
+        return render_template('editMenuItem.html',restaurant_id = restaurant_id, menu_id = menu_id, item=item)
+
     # return "This page is for editing menu item %s" %menu_id
 
 
-@app.route('/restaurants/restaurant_id/menu/menu_id/delete')
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET','POST'])
 # Delete the menu
-def deleteMenuItem():
-    return render_template('deleteMenuItem.html')
+def deleteMenuItem(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash("This menu item is successfully deleted!")
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('deleteMenuItem.html', item=itemToDelete )
+
     # return "This page is for deleting menu item %s" %menu_id
 
 
-
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
-
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8101)
+    app.run(port = 8101)
+
