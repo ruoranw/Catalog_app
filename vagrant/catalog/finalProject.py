78485d81c7c4a2609e0bbc8049d4af6aa7c3c65a
WangRuoran <wangruoran@WangRuorans-MacBook-Pro.local>
2017-05-08 12:18:02 -0700

    Finished google login and logout.

--- vagrant/catalog/finalProject.py
+++ vagrant/catalog/finalProject.py
@@ -41,6 +41,7 @@ def showLogin():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    # validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -58,9 +59,10 @@ def gconnect():
         return response
   # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('http://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
+    print result
   # If there was an error in the access token info, abort.
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
@@ -78,28 +80,37 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
-  # Check to see if the user is already logged in
-    stored_credentials = login_session.get('credentials')
+    stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                   200)
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
+
+
+
+  # # Check to see if the user is already logged in
+  #   stored_credentials = login_session.get('credentials')
+  #   stored_gplus_id = login_session.get('gplus_id')
+  #   if stored_credentials is not None and gplus_id == stored_gplus_id:
+  #       response = make_response(json.dumps('Current user is already connected.'),
+  #                                  200)
+  #       response.headers['Content-Type'] = 'application/json'
+  #       return response
 
   # Store the access token in the session for later use.
-    login_session['credentials'] = credentials
+    login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
   # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-    data = answer.json()
+    data = json.loads(answer.text)
+    print data
 
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
@@ -115,6 +126,66 @@ def gconnect():
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
+
+# Disconnect - Revoke a current user's token and rest their login_session
+@app.route("/gdisconnect")
+def gdisconnct():
+    # Only disconnect a connected user.
+    access_token = login_session['access_token']
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    if access_token is None:
+        print 'Access Token is None.'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is'
+    print result
+    if result['status'] == '200':
+        #Reset the user's session.
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # credentials = login_session.get('credentials')
+    # if credentials is None:
+    #     response = make_response(json.dumps('Current user not connected.'), 401)
+    #     response.headers['Content-Type'] = 'application/json'
+    #     return response
+    # Execute HTTP GET request to revoke current token.
+    # access_token = credentials.access_token
+    # url = 'http://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    # h = httplib2.Http()
+    # result = h.request(url, 'GET')[0]
+
+    # if result['status'] == '200':
+    #     #Reset the user's session.
+    #     del login_session['credentials']
+    #     del login_session['gplus_id']
+    #     del login_session['username']
+    #     del login_session['email']
+    #     del login_session['picture']
+    #     response = make_response(json.dumps('Successfully disconnected.'), 200)
+    #     response.headers['Content-Type'] = 'application/json'
+    #     return response
+    # else:
+    #     # For whatever reason, the given token was invalid
+    #     response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+    #     response.headers['Content-Type'] = 'application/json'
+    #     return response
 
 # Create an API Endpoint (Get Request)
 @app.route('/restaurants/JSON')
