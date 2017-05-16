27dc8cc36123fb28187f7bb0c3be9b2dab09f2ca
WangRuoran <wangruoran@WangRuorans-MacBook-Pro.local>
2017-05-16 13:06:59 -0700

    new changes on facebook connect and disconnect.

--- vagrant/catalog/finalProject.py
+++ vagrant/catalog/finalProject.py
@@ -136,7 +136,7 @@ def gconnect():
     print "done!"
     return output
 
-@app.route('/fbconnect', methods=['POST'])
+@app.route('/fbconnect/', methods=['POST'])
 def fbconnect():
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -148,15 +148,18 @@ def fbconnect():
     # Exchange client token for long-lived server-side token
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
     app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+    url = ('https://graph.facebook.com/v2.8/oauth/access_token?'
+           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
+           '&fb_exchange_token=%s') % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    # userinfo_url = "https://graph.facebook.com/v2.2/me"
     # Strip expire tag from access token
-    token = result.split("&")[0]
+    # token = result.split("&")[0]
+    data = json.loads(result)
+    token = 'access_token=' + data['access_token']
 
     url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
@@ -164,14 +167,16 @@ def fbconnect():
 
     print "url sent for API access:%s"% url
     print "API JSON result: %s" % result
+
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
+
     # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.2/me/picture?%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -195,10 +200,10 @@ def fbconnect():
         return output
 
 
-@app.route('/fbdisconnect')
+@app.route('/fbdisconnect/')
 def fbdisconnect():
     facebook_id = login_session['facebook_id']
-    access_token = login_session['access_token']
+    access_token = login_session.get('access_token')
     url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
@@ -482,5 +487,5 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(port = 8101)
+    app.run(host='0.0.0.0', port = 8101)
 
